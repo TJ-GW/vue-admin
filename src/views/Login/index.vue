3d5762a60413a51f4bd0fafb5d3795663c7fdf83
TJ-GW <761134933@qq.com>
2020-02-29 12:16:32 +0800

    项目构建，全局样式设置，登录路由设置，emui设置

---
+++ src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+    </div>
+</template>
+<script>
+    export default{
+        
+    }
+
+</script>
+// scoped 让样式只对当前页面生效
+<style lang="scss" scoped>
+    .login{
+       
+        height: 100vh;
+        background-color: #344a5f;
+       
+        
+    }
+</style>
